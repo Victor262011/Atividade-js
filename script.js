08f8545d222f22b82fa541998522bb4af9bbbbfd
VICTOR BARROS CARNEIRO <victor_carneiro@estudante.sesisenai.org.br>
2026-08-06 15:35:58 -0300

    Adição do exercício 2

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ document.getElementById("btn1").addEventListener("click", function () {
 //
 // Dica: use .style.color = "blue"
 
+document.getElementById("btn2").addEventListener("click", function () {
+    document.getElementById("texto2").style.color = "blue"
+})
 
 
 // ---------- Exercicio 3 ----------
