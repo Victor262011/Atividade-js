18ac0719194269a2d00cf9e8ef12b93d95248cba
VICTOR BARROS CARNEIRO <victor_carneiro@estudante.sesisenai.org.br>
2026-08-06 16:04:31 -0300

    Adição do exercício 6

--- script.js
+++ script.js
@@ -64,7 +64,12 @@ document.getElementById("btn5").addEventListener("click", function () {
 // Dica: leia o valor atual com .textContent, converta para
 // numero com Number() e some 1
 
+document.getElementById("btn6").addEventListener("click", function () {
+    const contador = document.getElementById("contador6")
+    const valorAtual = Number(contador.textContent)
 
+    contador.textContent = valorAtual + 1
+})
 
 // ---------- Exercicio 7 ----------
 // Ao clicar no botao, pegue o valor digitado no #input7
